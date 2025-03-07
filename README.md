4d6e2887adc2470f5a29ce52c2104859e5a7711d
JSubelj <jan.subelj010@gmail.com>
2025-03-07 21:10:16 +0100

    release

--- README.md
+++ README.md
@@ -1,6 +1,69 @@
 # vfs-pve-hook.sh
-## VirtioFs hook for Proxmox written in bash
+## VirtioFs hook for Proxmox written in Bash
 
+## Overview
+
+This script is a Proxmox hook script designed to simplify the management of virtiofs mounts for virtual machines. It automates the creation of virtiofs sockets and configures the necessary QEMU arguments within the Proxmox VM configuration.  It also handles logging, configuration loading, and cleanup.
+
+## Features
+
+* **Automated Virtiofs Setup:** Creates virtiofs sockets and configures QEMU arguments for specified shared directories.
+* **Flexible Configuration:** Reads configuration from `vfs-pve-hook.conf` to define shared paths, log levels, and additional virtiofs arguments per VM.
+* **NUMA Support:**  Dynamically adds or removes NUMA configuration based on settings in the configuration file.
+* **Logging:**  Provides detailed logging with different levels (DEBUG, INFO, WARN, ERROR) to `/dev/stdout` and `/dev/stderr`.
+* **Argument Backup:** Backs up existing QEMU arguments before modifying them. Restoring arguments automatically is not supported because Proxmox does not support reloading config after `pre-start` natively.
+* **Helper Script Output:** Prints a helper script with mount commands for the VM to easily mount the shared directories.
+* **Cleanup:** Removes virtiofs services after the VM stops.
+* **Handles Multiple Paths:** Supports multiple shared paths per VM, each with individual settings.
+* **Robust Error Handling:** Includes error checking and cleanup to ensure the system remains stable.
+
+## Prerequisites
+
+- Proxmox Virtual Environment (PVE)
+- `virtiofsd` executable located at `/usr/libexec/virtiofsd` (default install afaik)
+- Proper permissions to read/write Proxmox configuration files
+- `systemd` for managing VirtioFS services
+
+## Installation
+
+1. **Clone this repo:** `git clone https://github.com/JSubelj/vfs-pve-hook.sh.git vfs-pve-hook`
+2. **Create snippets dir for Proxmox:** `mkdir -p /var/lib/vz/snippets`
+3. **Copy script and make it executable to Proxmox dir:** `cp vfs-pve-hook/vfs-pve-hook.sh /var/lib/vz/snippets/ && chmod +x /var/lib/vz/snippets/vfs-pve-hook.sh`
+4. **Create [config](#Config):** `vim /var/lib/vz/snippets/vfs-pve-hook.conf` 
+5. **Configure the hook:** `qm set <vmid> -hookscript local:snippets/vfs-pve-hook.sh`
+
+## Config
+
+The file uses a simple colon separated key/value format, with sections. Each section is for its own VM. paths, loglevels and virtiofs_args are comma separated. NUMA can be enabled on all or on none.
+```ini
+[<vmid>]
+paths: /path/to/share1, /path/to/share2,...  # Comma-separated list of shared paths
+loglevel: debug, info,...  # Comma-separated list of log levels for each path, or a single log level for all. Defaults to 'info' if not set.
+virtiofs_args: -args for path 1-,-args for path 2-,... # Comma-separated list of additional virtiofs arguments for each path, or a single list for all.
+numa: true/false # Enable or disable NUMA support. Defaults to 'true' if not set.
+```
+
+## Usage
+
+The script is automatically called by Proxmox during VM lifecycle events (pre-start, post-start, pre-stop, post-stop). No manual intervention is required. It works like this:
+- **1st run**: Generates args and exits; Thats because Proxmox does not update config by default after pre-start
+- **subsequent runs**: Checks args, then sets everything up and exists.
+
+## Using other args on VM
+
+You can add other args to `<vmid>.conf` and everything should work fine. The only time you would need to manually remove args from this script is when changing some env vars in script itself. (But then I think you know what you're doing.)
+
+## Error codes
+
+- **255:** Configuration was generated and saved, because PVE does not reload config pre-start has to exit. You need to start vm again.
+- **1:** Unknown phase or other errors.
+- **3:** Failed to start VirtioFS service.
+- **5:** No configuration found for the VM.
+
+
+## License
+
+Is GNU GPL v3, because original Perl script is under that license and this script is a spiritual successor to that hookscript so I honored the license. [Sikha's post about license.](https://forum.proxmox.com/threads/virtiofsd-in-pve-8-0-x.130531/page-5#post-729223)
 
 ## Links
 - [Drallas's Mount Volumes into Proxmox VMs with Virtio-fs](https://gist.github.com/Drallas/7e4a6f6f36610eeb0bbb5d011c8ca0be)
--- vfs-pve-hook.sh
+++ vfs-pve-hook.sh
@@ -3,9 +3,10 @@ set -euo pipefail
 
 LOGLEVEL="DEBUG"
 DEFAULT_VFS_LOGLEVEL="info"
+DEFAULT_NUMA="true"
 
 VIRTIOFS_EXE="/usr/libexec/virtiofsd"
-SOCKET_DIR="/run/virtiofsd"
+SOCKET_DIR="/run/vfs-pve-hook"
 
 
 log() {
@@ -69,7 +70,7 @@ get_section_value() {
 }
 
 get_config() {
-    # This function is called: eval $(get_config paths loglevel virtiofs_args vm_args)
+    # This function is called: eval $(get_config paths loglevel virtiofs_args numa)
     # In those variables, data will be stored
 
     # Read paths and replace commas with spaces
@@ -83,10 +84,12 @@ get_config() {
     if [ -z "$loglevel" ]; then loglevel="$DEFAULT_VFS_LOGLEVEL"; fi
 
     local virtiofs_args=$(get_section_value "$VMID" virtiofs_args | sed 's/, */;/g')
-    local vm_args=$(get_section_value "$VMID" vm_args)
+    local numa=$(get_section_value "$VMID" numa)
+    if [ -z "$numa" ]; then numa="$DEFAULT_NUMA"; fi
+
 
     # Use eval to assign values to the output variables in the caller's scope
-    echo "$1=\"$paths\";$2=\"$loglevel\";$3=\"$virtiofs_args\";$4=\"$vm_args\""
+    echo "$1=\"$paths\";$2=\"$loglevel\";$3=\"$virtiofs_args\";$4=\"$numa\""
 }
 
 get_escaped_path(){
@@ -104,27 +107,66 @@ get_socket_path(){
 }
 
 
+move_old_args_files() {
+    # Initialize the suffix
+    suffix=1
+
+    base_file="$OLD_ARGS_FILE"
+
+    log INFO "Moving backup args config"
+    # Check if the base file exists and rename existing files incrementally
+    while [[ -e "$base_file" ]]; do
+        # Define the new file name with the next suffix
+        new_file="${base_file}.$suffix"
+
+        # If the new file name already exists, increment the suffix
+        while [[ -e "$new_file" ]]; do
+            ((suffix++))
+            new_file="${base_file}.$suffix"
+        done
+        log DEBUG "Moving $base_file into $new_file"
+        # Move the existing file to the new file name
+        mv "$base_file" "$new_file"
+
+        # Reset the suffix for the next iteration
+        suffix=1
+    done
+}
+
 setup_args_in_proxmox_config() {
     # Checks if args in config. If not adds them and STOPS execution! 
     # Copies old args into <vmid>.conf.old_args
     # If old args exist, 
 
-    local paths="$1"
-    local vm_args="$2"
-
      # Get args from proxmox config
     args_from_config=$(get_key_from_proxmox_config "args")
 
-    if [ -f "$OLD_ARGS_FILE" ]; then
-        log ERROR "Old args still exist in $OLD_ARGS_FILE. Check them and check config. Something didn't happen as expected."
-        exit 6
-    fi
-    log DEBUG "Args from config: $args_from_config"
-    echo "$args_from_config" > $OLD_ARGS_FILE
+    local touching_args="false";
+    local memory=$(get_key_from_proxmox_config "memory")
 
-    memory=$(get_key_from_proxmox_config "memory")
-    # Generating object section of args:
-    local args="-object memory-backend-file,id=mem,size=${memory}M,mem-path=/dev/shm,share=on -numa node,memdev=mem"
+    local args="$args_from_config" 
+
+    local memory_part_of_args="-object memory-backend-file,id=mem,size=${memory}M,mem-path=/dev/shm,share=on"
+    if [[ "$args" != *"$memory_part_of_args"* ]]; then
+        log DEBUG "Memory part of args missing, adding to generate args"
+        args+=" $memory_part_of_args"
+        touching_args="true"
+    fi
+
+    local numa_part_of_args="-numa node,memdev=mem"
+    if [[ "$numa" == "true" && "$args" != *"$numa_part_of_args"* ]]; then
+        log DEBUG "Including NUMA"
+        args+=" $numa_part_of_args"
+        touching_args="true"
+    fi
+
+    if [[ "$numa" != "true" && "$args" == *"$numa_part_of_args"* ]]; then
+        log DEBUG "Removing NUMA"
+        args=${args//$numa_part_of_args/}
+        touching_args="true"
+    fi
+
+
     IFS=';'
     read -r -a paths <<< "$paths_all"
     for path in "${paths[@]}"; do
@@ -132,54 +174,52 @@ setup_args_in_proxmox_config() {
         escapedpath=$(get_escaped_path "$path")
         log DEBUG "Escaped path '$escapedpath'"
         # generating chardev
-        chardev="-chardev socket,id=char_${VMID}_${escapedpath},path=$(get_socket_path $escapedpath)"
-        log DEBUG "Chardev: '$chardev'"
+        local chardev_part_of_args="-chardev socket,id=char_${VMID}_${escapedpath},path=$(get_socket_path $escapedpath)"
+        log DEBUG "Chardev: '$chardev_part_of_args'"
+        if [[ "$args" != *"$chardev_part_of_args"* ]]; then
+            log DEBUG "Including chardev for $path"
+            args+=" $chardev_part_of_args"
+            touching_args="true"
+        fi
         # generating device
         local tag="$VMID-$escapedpath"
-        device="-device vhost-user-fs-pci,queue-size=1024,chardev=char_${VMID}_${escapedpath},tag=$tag"
-        log DEBUG "Device: '$device'"
-        args="$args $chardev $device"
+        local device_part_of_args="-device vhost-user-fs-pci,queue-size=1024,chardev=char_${VMID}_${escapedpath},tag=$tag"
+        log DEBUG "Device: '$device_part_of_args'"
+        if [[ "$args" != *"$device_part_of_args"* ]]; then
+            log DEBUG "Including device for $path"
+            args+=" $device_part_of_args"
+            touching_args="true"
+        fi
     done
-
-    if [[ -n "$args_from_config" && "$args_from_config" != "$args" ]]; then
-        log DEBUG "Adding args from config"
-        args="$args_from_config $args"
-    fi
-
-    if [ ! -z "$vm_args" ]; then
-        args="$args $vm_args"
-    fi
 
     prettyargs=$(echo "$args" | sed 's/ -/\n-/g; s/^-/\n-/g; s/\n/\n\t/g;' )
     log INFO "Final vm args are: $prettyargs"
 
-    log INFO "Writting args into proxmox config"
+    
 
-    #pvesh set /nodes/fangorn/qemu/100/config --args "$args"
+    # I didn't want to change config with sed, because it's not really a supported mode. Via bash api would timeout for lock only perl way works.
+    if [[ "$touching_args" == "true" ]]; then
+        move_old_args_files
+        log INFO "Writing args to $OLD_ARGS_FILE"
+        echo "$args_from_config" > $OLD_ARGS_FILE
 
-    # Couldn't find any other way only in perl!
-    # LMAO skripta ki se izvede uresnice ne naloži nove config ampak ohranja staro, ker se stara ne pobriše!!!
-    # Treba je narest setup, ki setupa argumente!!
-    # vm_args ne rabjo obstajat!
-    # Mogoče pa spodnje deluje (torej upošteva args:) samo če je args: tudi v prejšni iteraciji configa. 
-    # če ne, ga ne upoštvea??
-    perl -e "
+        log INFO "Writting args into proxmox config"
+
+        perl -e "
         use PVE::QemuServer;
         my \$conf = PVE::QemuConfig->load_config($VMID);
-        \$conf->{args} = \" $args\";
+        \$conf->{args} = \"$args\";
         PVE::QemuConfig->write_config($VMID, \$conf);
-    "
-    
-    log INFO "Writting to config successful."
+        "
 
-
+        log WARN "Args were written. Because Proxmox does not reload config after pre-start (unless you install a patch that does this), this script will exit and you will need to qm start your VM again."
+        exit -1
+    fi
+    log INFO "Writting to config not needed. Continuing with setup."
 }
 
 
 setup_virtiofs_sockets() {
-    local paths_all="$1"
-    local loglevel_all="$2"
-    local virtiofs_args_all="$3"
     IFS=';'
     read -r -a paths <<< "$paths_all"
     read -r -a loglevels <<< "$loglevel_all"
@@ -197,8 +237,8 @@ setup_virtiofs_sockets() {
     
     local i=0
     for ((i=0; i<${#paths[@]}; i++)); do
-        p="${paths[$i]}"
-        escapedpath=$(get_escaped_path "$p")
+        local p="${paths[$i]}"
+        local escapedpath=$(get_escaped_path "$p")
 
         if [ ! -z $loglevel ]; then 
             log DEBUG "Setting provided loglevel for all - in loop"
@@ -208,12 +248,12 @@ setup_virtiofs_sockets() {
             log DEBUG "Setting specific loglevel for vfs $i to $ll"
         fi
 
-        vfs_args="${virtiofs_args[$i]:-}"
-        service_name="virtiofs-$VMID-${escapedpath}"
-        socket_path="$(get_socket_path $escapedpath)"
+        local vfs_args="${virtiofs_args[$i]:-}"
+        local service_name="virtiofs-$VMID-${escapedpath}"
+        local socket_path="$(get_socket_path $escapedpath)"
 
         mkdir -p "$SOCKET_DIR"
-        log INFO "Creating socket '$socket_path' with unit name '$service_name' for '$p' with loglevel: '$ll' and additional vfsargs: '$vfs_args'"
+        log INFO "Creating socket '$socket_path' with unit name '$service_name' for '$p' with loglevel: '$ll' and additional vfs args: '$vfs_args'"
         
         local service_command="systemd-run \
                 --unit=\"$service_name\" \
@@ -229,17 +269,27 @@ setup_virtiofs_sockets() {
         fi
         set +e
         eval "$service_command"
-            
-        log INFO "Checking if service was created"
-        systemctl --no-pager status "$service_name"
         if [ $? -ne 0 ]; then
             log ERROR "Can't start service. Cleaning up."
-            systemctl disable $service_name
-            systemctl reset-failed $service_name
+            systemctl stop "$service_name"
+            systemctl disable "$service_name"
+            systemctl reset-failed "$service_name"
+            log INFO "Exiting..."
+            exit 3
+        fi
+
+        log INFO "Checking if service was created"
+        systemctl --no-pager status "$service_name" 
+        if [ $? -ne 0 ]; then
+            log ERROR "Can't start service. Cleaning up."
+            systemctl stop "$service_name"
+            systemctl disable "$service_name"
+            systemctl reset-failed "$service_name"
             log INFO "Exiting..."
             exit 3
         fi
         set -e
+        ls "$socket_path"
     done
         
 }
@@ -276,38 +326,19 @@ print_helper_script_for_mnt() {
 
 pre_start() {
     log INFO "Setuping argument in Proxmox config"
-    setup_args_in_proxmox_config "$paths_all" "$vm_args"
+    setup_args_in_proxmox_config 
 
     log INFO "Creating virtiofs socket(s)."
-    setup_virtiofs_sockets "$paths_all" "$loglevel_all" "$virtiofs_args_all"
-}
-
-restore_args(){
-    local args=$(cat "$OLD_ARGS_FILE")
-
-    log INFO "Restoring args."
-    local escaped_args=${args//\//\\/}
-    log DEBUG "Args to restore: '$args'"
-    if [ -z "$args" ]; then 
-        log DEBUG "Removing current args."
-        sed "/^args:/d" -i "$PROXMOX_CONFIG"
-    else
-        log DEBUG "Replacing old args."
-        sed "s/^args:.*/args: $escaped_args/g" -i "$PROXMOX_CONFIG"
-    fi
-    rm "$OLD_ARGS_FILE"
-    log INFO "Args restored succesfully"
+    setup_virtiofs_sockets 
 }
 
 post_start() {
     print_helper_script_for_mnt
-    restore_args
 }
 
 remove_virtiofs_services() {
     IFS=';'
     read -r -a paths <<< "$paths_all"
-
     for ((i=0; i<${#paths[@]}; i++)); do
         p="${paths[$i]}"
         escapedpath=$(get_escaped_path "$p")
@@ -339,14 +370,17 @@ phase="$2"
 PROXMOX_CONFIG_DIR="/etc/pve/qemu-server"
 PROXMOX_CONFIG="$PROXMOX_CONFIG_DIR/$VMID.conf"
 
-OLD_ARGS_FILE="/run/$VMID.before_virtfs_args"
+OLD_ARGS_FILE="$PROXMOX_CONFIG_DIR/$VMID.conf.old_args"
 RUNTIME_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONF_FILE="$RUNTIME_DIR/vfs-pve-hook.conf"
 
+if [ -f "$RUNTIME_DIR/.env" ]; then
+    . "$RUNTIME_DIR/.env"
+fi
 
 # Call get_config with variable names to store the results
-ret=$(get_config paths_all loglevel_all virtiofs_args_all vm_args)
-if [ ! $? -eq 0 ]; then log ERROR "Error when getting config for '$VMID'. Exiting..."; return 1; fi;
+ret=$(get_config paths_all loglevel_all virtiofs_args_all numa)
+if [ ! $? -eq 0 ]; then log ERROR "Error when getting config for '$VMID'. Exiting..."; exit 6; fi;
 eval "$ret"
 
 
@@ -369,7 +403,7 @@ case "$phase" in
         # Post start restores proxmox config back to previous conf.
 
         log INFO "$VMID started successfully."
-        #post_start
+        post_start
         
         ;;
 
